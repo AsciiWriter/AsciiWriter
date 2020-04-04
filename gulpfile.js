49c76a3b860917e3f6f35ce9de2334f9aedee360
ojn <gleb@netkom.se>
2020-04-05 00:50:00 +0200

    tagged gulpfile, rename to asciiwriter docs, remove highlight class in highlight-code.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 'use strict';
-
+// tag::require[]
 const gulp = require('gulp');
-var sass = require('gulp-dart-sass');
-
+const sass = require('gulp-dart-sass');
 const website = require('./index')({path: '../'});
+// end::require[]
+
 
 // tag::hbs-partials[]
 const HANDLEBARS_PARTIALS = [
@@ -14,6 +15,7 @@ const HANDLEBARS_PARTIALS = [
 ];
 // end::hbs-partials[]
 
+// tag::articles-indexing[]
 gulp.task('articles-indexing', () =>
   gulp.src('src/articles/**/*.adoc')
       .pipe(website.readAsciidoc())
@@ -21,21 +23,27 @@ gulp.task('articles-indexing', () =>
       .pipe(website.convertToJson('articlesindex.json'))
       .pipe(gulp.dest('build/.tmp'))
 );
+// end::articles-indexing[]
 
+// tag::articles-rss[]
 gulp.task('articles-rss', () =>
   gulp.src('build/.tmp/articlesindex.json')
       .pipe(website.readIndex())
       .pipe(website.convertToRss('articles.xml'))
       .pipe(gulp.dest('build/docs/rss'))
 );
+// end::articles-rss[]
 
+// tag::articles-list[]
 gulp.task('articles-list', () =>
   gulp.src('build/.tmp/articlesindex.json')
       .pipe(website.readIndex())
       .pipe(website.convertToArticlesList('src/templates/site.hbs', HANDLEBARS_PARTIALS, 'site.html', 1))
       .pipe(gulp.dest('build/docs'))
 );
+// end::articles-list[]
 
+// tag::articles-page[]
 gulp.task('articles-page', (cb) => {
   gulp.src('src/articles/**/*.adoc')
       .pipe(website.readAsciidoc())
@@ -46,46 +54,57 @@ gulp.task('articles-page', (cb) => {
       .pipe(gulp.dest('build/docs/articles'))
       .on('end', () => cb())
 });
+// end::articles-page[]
 
 gulp.task('articles', gulp.series('articles-indexing', 'articles-page', 'articles-list', 'articles-rss'), cb => cb());
 
-// html task:
+// tag::html-task[]
+// tag::html-indexing[]
 gulp.task('html-indexing', () =>
   gulp.src(`src/html/**/*.html`)
       .pipe(website.readHtml())
       .pipe(website.convertToJson('pageindex.json'))
       .pipe(gulp.dest('build/.tmp')));
-
+// end::html-indexing[]
+// tag::html-template[]
 gulp.task('html-template', () =>
   gulp.src(`src/html/**/*.html`)
       .pipe(website.readHtml())
       .pipe(website.applyTemplate(`src/templates/site.hbs`, HANDLEBARS_PARTIALS))
       .pipe(gulp.dest('build/.tmp'))
       .pipe(gulp.dest('build/docs')));
-
+// end::html-template[]
 gulp.task('html', gulp.parallel('html-indexing', 'html-template'));
+// end::html-task[]
 
-// assets task:
+// tag::404page[]
 gulp.task('404page', () =>
   gulp.src(`src/404/404.html`)
       .pipe(gulp.dest('build/.tmp'))
       .pipe(gulp.dest('build/docs')));
-      
+// end::404page[]
+
+// tag::img[]      
 gulp.task('img', () =>
   gulp.src(`src/img/**/*`)
       .pipe(gulp.dest('build/.tmp/img'))
       .pipe(gulp.dest('build/docs/img')));
+// end::img[]
 
-gulp.task('css', () =>
-  gulp.src(`src/css/**/*`)
-      .pipe(gulp.dest('build/.tmp/css'))
-      .pipe(gulp.dest('build/docs/css')));
-
+// tag::sass[]
 gulp.task('sass', function () {
   return gulp.src(`src/sass/**/*.scss`)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('src/css'));
 });
+// end::sass[]
+
+// tag::css[]
+gulp.task('css', () =>
+  gulp.src(`src/css/**/*`)
+      .pipe(gulp.dest('build/.tmp/css'))
+      .pipe(gulp.dest('build/docs/css')));
+// end::css[]
 
 gulp.task('js', () =>
   gulp.src(`src/js/**/*`)
@@ -94,13 +113,16 @@ gulp.task('js', () =>
 
 gulp.task('assets', gulp.parallel('404page', 'img', 'css', 'js'));
 
+// tag::sitemap[]
 gulp.task('sitemap', () =>
   gulp.src(['build/.tmp/articlesindex.json', 'build/.tmp/pageindex.json'])
       .pipe(website.readIndex())
       .pipe(website.convertToSitemap())
       .pipe(gulp.dest('build/docs'))
 );
+// end::sitemap[]
 
+// tag::check[]
 gulp.task('check', () =>
   gulp.src([ // We must have a articles index page
              'build/.tmp/articlesindex.json',
@@ -120,5 +142,8 @@ gulp.task('check', () =>
       .pipe(website.extVerifyFiles())
       .pipe(gulp.dest('build/check'))
 );
+// end::check[]
 
+// tag::default[]
 gulp.task('default', gulp.series('assets', 'articles', 'html', 'sitemap', 'check'), cb => cb());
+// end::default[]
--- src/code/ext/highlight-code.js
+++ src/code/ext/highlight-code.js
@@ -12,7 +12,7 @@ module.exports = function ({ selector }) {
       const elem = $(code);
       const language = elem.prop('data-lang');
       const fileContents = elem.html();
-      elem.parent().replaceWith( `<code class="language-${language}">${fileContents}</code>`);
+      elem.parent().replaceWith( `<pre  class="highlight"><code class="language-${language}">${fileContents}</code></pre>`);
     });
 
     file.contents = Buffer.from($.html());
